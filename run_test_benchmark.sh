053caa5f79a3790b94690bd9bb4fe735635604fd
anonymousauthorCV <74913773+anonymousauthorCV@users.noreply.github.com>
2021-04-19 20:38:48 +0800

    Update run_test_benchmark.sh

--- run_test_benchmark.sh
+++ run_test_benchmark.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-CUDA_VISIBLE_DEVICES=7 python main_benchmark.py --datapath data/completion3d --model_dir trained_model --mode 1 --dataset PCN --batch_size 1 --num_points 16384 --model_name Model --log_env PCN --lr 0.0001 --loss CD --use_mean_feature 0 --workers 16 --nepoch 300
+CUDA_VISIBLE_DEVICES=7 python main_benchmark.py --datapath data/completion3d --model_dir trained_model --mode 1 --dataset Completion3D --batch_size 1 --num_points 16384 --model_name Model --log_env PCN --lr 0.0001 --loss CD --use_mean_feature 0 --workers 16 --nepoch 300
